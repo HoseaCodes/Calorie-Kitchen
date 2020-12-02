a0741e63fad336531fdef8f65238d8be36cddb45
hosead6168 <dominique11h@yahoo.com>
2020-12-02 00:30:50 -0600

    updated api key

--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    RECIPE_API_KEY: "e6969ac5345cc8efaebe1e262d4c8a7d"
-}
-
-//"https://api.spoonacular.com/mealplanner/generate?timeFrame=<string>&targetCalories=<number>&diet=<string>&exclude=<string>"
-
