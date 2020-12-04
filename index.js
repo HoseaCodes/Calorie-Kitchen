a44a608ed1db42b96fcd0f00ab161be8ef55ad60
hosead6168 <dominique11h@yahoo.com>
2020-12-04 17:14:04 -0600

    Deploy to Heroku

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
 app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.render('index');
+});
