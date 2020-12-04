ca3f1dde8aeea1d081fdcd97e0a711c3c9a1937c
hosead6168 <dominique11h@yahoo.com>
2020-12-04 17:01:51 -0600

    port to origin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-console.log(process.env)
-app.listen(3000, () => console.log('Listening on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port}`));
 app.use(express.static('public'));
